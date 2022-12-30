6efb5225d2ce3bc6748d4489ddaa91026071616d
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2022-12-30 20:46:21 -0300

    Aula sobre anatomia de classes

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,24 @@
+public class MinhaClasse {
+    
+    public static void main(String []args){
+        //System.out.print ("Olá turma, sejam bem-vindos!");
+
+        //Variável ano pode sofrer alteração
+        int ano = 2022;
+        ano = 2023;
+
+        //Variável toda maiúscula sinaliza que ela é
+        //final e não pode ser alterada
+        final String BR = "Brasil";
+
+        //BR = "Brazil"; Erro
+
+        String primeiroNome = "Danielle";
+        String segundoNome = "Victória";      
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+}
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do método " +  primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
