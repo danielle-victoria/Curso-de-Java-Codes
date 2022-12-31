ec57d8b47954c8283b36c9b8b397a208fa3fe3a9
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2022-12-30 22:40:09 -0300

    Aula 2 de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,7 @@
 public class Operadores {
 
     public static void main(String[] args){
+        /*Aula 1 de Operadores
         //Ocorre uma concatenação
         String nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);  //Saída: LINGUAGEMJAVA
@@ -21,7 +22,27 @@ public class Operadores {
         System.out.println(concatenacao); //Saída:1111
 
         concatenacao = "1"+(1+1+1); 
-        System.out.println(concatenacao); //Saída:13
+        System.out.println(concatenacao); //Saída:13*/
+
+
+
+
+        //Aula 2 de Operadores
+
+        int numero = 5;
+        System.out.println(-numero); //-5
+        System.out.println(numero); //5
+
+        //Declarando um número negativo
+
+        numero = - numero;
+        System.out.println(numero); //-5
+
+        //Deixando um número positivo novamente
+
+        //numero = + numero; //Errado numero = -5
+        numero = numero * -1; // Resolve o problema acima
+        System.out.println(numero);
     
     }
     
