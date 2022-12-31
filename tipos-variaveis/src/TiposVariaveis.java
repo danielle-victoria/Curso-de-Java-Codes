e82cc1b2c8b73247b24ad3010c1522a60ba245da
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2022-12-30 21:52:00 -0300

    Adição de informações sobre os tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500.33;  
+        float pi = 3.14F;  //sempre colocar o F no fim para a variável ser entendida como float e não double
+        long cpf = 98765432109L; //tipo long precisa ser encerrado com um "L" no final
+    
+        //Java é fortemente tipado.
+
+        //Exemplo
+
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //short numeroCurto2 = numeroNormal; //Erro
+        short numeroCurto2 = (short) numeroNormal; //Solução para o problema acima
+
+
     }
 }
