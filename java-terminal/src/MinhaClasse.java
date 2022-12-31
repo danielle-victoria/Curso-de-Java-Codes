2624fbd93c409dc373d2c9d44ff33f15cd822291
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2022-12-31 12:47:20 -0300

    Aula 1 sobre terminal e argumentos

---
+++ java-terminal/src/MinhaClasse.java
@@ -0,0 +1,6 @@
+//Aula 1 sobre terminal e argumentos
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi Turma, fui executado pelo Terminal");
+    }
+}
