dc8a4bbaa05a77401b04c3c93e2835003f29578a
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2022-12-30 21:10:06 -0300

    Aula sobre indentação

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,24 @@
+public class BoletimEstudantil {
+    public static void main (String[] args){
+
+
+        //Sem indentação
+        /*int mediaFinal = 6;
+        if(mediaFinal<6)	
+        System.out.println("REPROVADO"); 
+        else if(mediaFinal==6)
+        System.out.println("PROVA MINERVA"); 
+        else
+        System.out.println("APROVADO"); */
+
+        //Com indentação
+        int mediaFinal = 7;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO"); 
+
+    }
+}
