2bbc502d690e36298340d937275c4512ba8bf8e2
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 23:53:10 -0300

    Aula de exercícios - Arrays - Ex4

--- arrays/src/exercicio4/ArrayMultidimensional.java
+++ arrays/src/exercicio4/ArrayMultidimensional.java
@@ -4,6 +4,27 @@
 
 package exercicio4;
 
+import java.util.Random;
+
 public class ArrayMultidimensional {
     
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] Matriz = new int[4][4];
+
+        for(int i= 0 ; i < Matriz.length; i++){
+            for(int j = 0; j < Matriz[i].length; j++ ){
+                Matriz[i][j] =  random.nextInt(9);
+
+            }
+        }
+        System.out.println("Matriz: ");
+        for(int[] linha : Matriz){
+            for(int coluna : linha){
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
 }
