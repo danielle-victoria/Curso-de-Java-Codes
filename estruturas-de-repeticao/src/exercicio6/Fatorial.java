b783dbcb3190292a6974629cdda6b6190aed7e79
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 22:37:58 -0300

    Aula de exercícios - Estruturas de Repetição - Ex6

--- estruturas-de-repeticao/src/exercicio6/Fatorial.java
+++ estruturas-de-repeticao/src/exercicio6/Fatorial.java
@@ -1,5 +1,28 @@
+/*Faça um programa que calcule o fatorial de um 
+número inteiro fornecido pelo usuário.
+Ex.: 5!=5.4.3.2.1=120 */
+
 package exercicio6;
+import java.util.Scanner;
 
 public class Fatorial {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Fatorial: ");
+        int fatorial = scan.nextInt();
+        int multiplicacao = 1;
+
+        System.out.print(fatorial + "!=");
+        for(int i = fatorial; i >= 1; i--){
+            multiplicacao *= i; 
+            if( i == 1) System.out.print(i);
+            else System.out.print(i + ".");           
+        }
+        
+        System.out.println("=" + multiplicacao);
+    }
     
 }
