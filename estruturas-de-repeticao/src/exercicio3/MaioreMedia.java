39b8809019192492b7bd9de36ed8f921636b31f8
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 21:43:51 -0300

    Aula de exercícios - Estruturas de Repetição - Ex3

--- estruturas-de-repeticao/src/exercicio3/MaioreMedia.java
+++ estruturas-de-repeticao/src/exercicio3/MaioreMedia.java
@@ -1,5 +1,41 @@
+/* Faça um programa que leia 5 números e informe 
+o maior número e a média desses números. */
+
 package exercicio3;
+import java.util.Scanner;
 
 public class MaioreMedia {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int count = 0;
+        int maior = 0;
+        int soma = 0;
+        double media = 0d;
+
+
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            soma += numero;
+
+            if(numero > maior)
+                maior = numero;
+                
+            count += 1;
+
+        } while(count < 5);
+
+        media = soma / count;
+
+        System.out.println("Maior número: " + maior);
+        System.out.println("Média: " + media);
+
+        
+    }
     
 }
