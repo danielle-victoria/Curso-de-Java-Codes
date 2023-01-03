ed699109fb7dfcf4ec7dd25fc9565c03755cfed7
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 23:03:46 -0300

    Aula de exercícios - Arrays - Ex1

---
+++ arrays/src/exercicio1/OrdemInversa.java
@@ -0,0 +1,28 @@
+/* Faça um Programa que leia um vetor de 6 números
+ inteiros e mostre-os na ordem inversa. */
+
+package exercicio1;
+import java.util.Scanner;
+public class OrdemInversa {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        int[] numeros = {-5, -6, 15, 50, 8, 4};
+        int count = 0;
+
+        //System.out.println(numeros.length); // tamanho do vetor
+        System.out.println("Vetor: ");
+        while(count < (numeros.length)){
+            System.out.print(numeros[count] + " ");
+            count++;
+            
+        }
+        
+        //Ordem Inversa
+        System.out.println("\nOrdem Inversa: ");
+        for(int i = numeros.length-1; i >= 0; i--){
+            System.out.print(numeros[i] + " ");
+        }
+        
+    }
+}
