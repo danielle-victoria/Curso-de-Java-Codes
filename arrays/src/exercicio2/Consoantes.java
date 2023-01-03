3d3bf02d3d47111be7d44a1b9c75c2a234338ad7
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 23:27:56 -0300

    Aula de exercícios - Arrays - Ex2

--- arrays/src/exercicio2/Consoantes.java
+++ arrays/src/exercicio2/Consoantes.java
@@ -2,7 +2,46 @@
  e diga quantas consoantes foram lidas. Imprima as consoantes. */
 
 package exercicio2;
+import java.util.Scanner;
 
 public class Consoantes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int quantidadeConsoantes = 0;
+        int count = 0;
+
+
+        do{
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if(! (letra.equalsIgnoreCase("a") ||
+               letra.equalsIgnoreCase("e") ||
+               letra.equalsIgnoreCase("i") ||
+               letra.equalsIgnoreCase("o") ||
+               letra.equalsIgnoreCase("u"))){
+                
+                
+                consoantes[count] = letra;
+                quantidadeConsoantes++;
+
+               }
+
+               count++;
+
+        }while(count < consoantes.length);
+
+        System.out.print("Consoantes: ");
+        //Para cada elemento dentro do array consoantes, imprima esse elemento
+        for(String consoante: consoantes){ //for-each
+            if(consoante != null)
+                System.out.print(consoante + " ");
+
+        }
+
+        System.out.println("\nQuantidade de consoantes lidas: " + quantidadeConsoantes);
+    }
     
 }
