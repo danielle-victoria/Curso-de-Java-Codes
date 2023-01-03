c87c119c821ad68b0d30eea22d2030efc5cea585
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 21:59:01 -0300

    Aula de exercícios - Estruturas de Repetição - Ex4

--- estruturas-de-repeticao/src/exercicio4/ParImpar.java
+++ estruturas-de-repeticao/src/exercicio4/ParImpar.java
@@ -1,5 +1,43 @@
+/*Faça um programa que peça N números inteiros. 
+Calcule e mostre a quantidade de números pares e
+a quantidade de números impares. */
+
+
 package exercicio4;
+import java.util.Scanner;
 
 public class ParImpar {
+
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        int quantidadeNumeros;
+        int numero;
+        int count = 0;
+        int quantidadePares = 0, quantidadeImpares = 0;
+
+
+        System.out.println("Quantidade de números: ");
+        quantidadeNumeros = scan.nextInt();
+
+        do{
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+            count++;
+
+            if(numero % 2 == 0){ //Verifica se o número é par
+                quantidadePares++;
+            }else{
+                quantidadeImpares++;
+            }
+
+        }while(count < quantidadeNumeros);
+
+        System.out.println("Quantidade de números pares: " + quantidadePares);
+        System.out.println("Quantidade de números ímpares: " + quantidadeImpares);
+
+
+    }
     
 }
