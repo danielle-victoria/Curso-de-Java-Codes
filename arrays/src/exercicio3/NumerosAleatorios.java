f17e88dbf854458a9e175d88a41bcf2592e9a2e0
danielle-victoria <danielle.victoria.eustaquio@gmail.com>
2023-01-02 23:40:08 -0300

    Aula de exercícios - Arrays - Ex3

--- arrays/src/exercicio3/NumerosAleatorios.java
+++ arrays/src/exercicio3/NumerosAleatorios.java
@@ -1,5 +1,36 @@
+/*Faça um Programa que leia 20 números inteiros aleatórios 
+(entre 0 e 100) armazene-os num vetor. Ao final, mostre os
+ números e seus sucessores. */
+
 package exercicio3;
+import java.util.Random;
+import java.util.Scanner;
 
 public class NumerosAleatorios {
+    public static void main(String[] args) {
+        
+        //Classe para gerar números aleatórios
+        Random random = new Random(); 
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < (numerosAleatorios.length-1); i++){
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.print("Números Aleatórios: ");
+        //for-each -> Recomendado para imprimir os dados
+        for(int numero: numerosAleatorios){
+            System.out.print(numero + " ");
+        }
+
+
+        System.out.print("\nSucessores dos Números Aleatórios: ");
+        //for-each -> Recomendado para imprimir os dados
+        for(int numero: numerosAleatorios){
+            System.out.print((numero + 1) + " ");
+        }
+    }
     
 }
